f963218c2a80a6db8d9e857403f4348e111a0317
IT24103089 <it24103089@my.sliit.lk>
2024-09-24 00:49:24 +0530

    Lab7Q2A, Lab7Q2B, Lab7Q2C

---
+++ Q2/IT24103089Lab7Q2A.java
@@ -0,0 +1,10 @@
+public class IT24103089Lab7Q2A {
+    public static void main (String[]args) {
+     int i = 0
+     while (i <4 ) {
+       System.out.println("$ $ $ $");
+       i = i +1;
+     }
+    }
+}
+     
---
+++ Q2/IT24103089Lab7Q2C.java
@@ -0,0 +1,12 @@
+public class IT24103089Lab7Q2C {
+    public static void main (String[]args) {
+
+      for ( int i = 5 ; i >0 ; i -- ) {
+           for ( int j = 0 ; j < i ; j ++ ) {
+             System.out.print( i );
+           }
+
+         System.out.println (" ");
+      }
+    }
+}
